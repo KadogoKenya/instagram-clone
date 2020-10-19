4bbdfe5070e3e744d9c39c9032017b5e8e3f56d2
Kate <you@example.com>
2020-10-19 06:18:33 +0300

    url redirection

--- Instagram/urls.py
+++ Instagram/urls.py
@@ -4,8 +4,6 @@ from . import views
 from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 
 urlpatterns = [
-    # path('', views.home, name='insta-home'),
-    # path('like/<int:pk>',views.likePost, name="likePost"),
     url(r'^like/(\d+)',views.likePost, name="likePost"),
     path('about/', views.about, name="insta-about"),
     path('', PostListView.as_view(), name='insta-home'),
